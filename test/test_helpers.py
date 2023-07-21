6a7a08ed16ca94b2aeab0029cee99f5a0bdbe3b5
Felix Exner <git@fexner.de>
2023-07-21 17:28:27 +0200

    Added test file for helper functions

---
+++ test/test_helpers.py
@@ -0,0 +1,30 @@
+import os
+
+from catmux.session import check_boolean_field
+from catmux.tmux_wrapper import _safe_call as safe_call
+
+
+def test_boolean_field():
+    assert check_boolean_field("yes") == True
+    assert check_boolean_field("Yes") == True
+    assert check_boolean_field("YES") == True
+    assert check_boolean_field("true") == True
+    assert check_boolean_field("True") == True
+    assert check_boolean_field("TRUE") == True
+    assert check_boolean_field("t") == True
+    assert check_boolean_field("T") == True
+    assert check_boolean_field("1") == True
+    assert check_boolean_field("no") == False
+    assert check_boolean_field("No") == False
+    assert check_boolean_field("NO") == False
+    assert check_boolean_field("false") == False
+    assert check_boolean_field("False") == False
+    assert check_boolean_field("FALSE") == False
+    assert check_boolean_field(True) == True
+    assert check_boolean_field(False) == False
+
+
+def test_safe_call():
+    assert safe_call(["bash", "-c", "exit 0"]) == True
+    assert safe_call(["bash", "-c", "exit 1"]) == False
+    assert safe_call(["bash", "-c", "return 0"]) == False  #  illegal bash
