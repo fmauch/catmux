ec3ca5bf1a6213a10eac8db5beb326b44d6e3fb6
Felix Exner <git@fexner.de>
2024-01-29 20:43:38 +0100

    Bump version for upcoming release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="catmux",
-    version="0.3.4",
+    version="0.3.5",
     license="MIT",
     author="Felix Exner",
     author_email="felix_mauch@web.de",
